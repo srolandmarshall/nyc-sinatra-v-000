4e3d60f3a5ecacaec6275ce7cb406e341e54b57f
Sam Marshall <samuel.r.marshall@gmail.com>
2017-11-12 12:08:57 -0500

    what I did during CoC meeting

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,26 @@
+class FiguresController < ApplicationController
+
+    get '/figures/new' do
+      erb :'/figures/new'
+    end
+
+    get '/figures' do
+      @figures = Figure.all
+      erb :'/figures/index'
+    end
+
+    post '/figures' do
+      @figure = Figure.create(params[:figure])
+      @figure.titles << Title.create(params[:title]) if !params[:title].empty?
+      @figure.landmarks << Landmark.create(params[:landmark]) if !params[:landmark].empty?
+      @figure.save
+      # binding.pry:
+      redirect '/figures/'+@figure.id.to_s
+    end
+
+    get '/figures/:id' do
+      @figure = Figure.find_by(id: params[:id])
+      erb :'/figures/show'
+    end
+
+end
